724656cd267c98eac6cc95de67298364a2902e34
primus <prmus@solbox.com>
2018-04-10 16:57:08 +0900

    mistyping

--- stringutil.go
+++ stringutil.go
@@ -1,6 +1,6 @@
 package stringutil
 
-func Reverse(s string) strin {
+func Reverse(s string) string {
 	r := []runes(s)
 	for i,j := 0, len(r)-1; i<len(r)/2;i,j = i+1, j-1 {
 			r[i], r[j] = r[j], r[i]A
